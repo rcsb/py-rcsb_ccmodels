d1875325849897556d87da5777b7f14dfbde1f90
jwest <john.westbrook@rcsb.org>
2021-01-27 09:25:43 -0500

    Adding example Linux settings to environmental setup scripts

--- ccdc-api-env.sh
+++ ccdc-api-env.sh
@@ -5,17 +5,46 @@
 #  Adjust these for settings for the site specific install of the
 #  CSD database and the CCDC Python API
 #
-#  Note:  the vscode defintions in ./.env
-export CSDHOME=/Applications/CCDC/CSD_2021
-PYROOT=$PYENV_ROOT/versions/3.7.9
-export CSD_PYTHON_ROOT_PATH=$PYROOT
-#
-# For Linux
-export LD_LIBRARY_PATH=$PYROOT/lib:$PYROOT/lib/python3.7/site-packages/ccdc/_lib:$LD_LIBRARY_PATH
-# For Macos
-export DYLD_FRAMEWORK_PATH=$PYROOT/lib/python3.7/site-packages/ccdc/_lib
-export DYLD_LIBRARY_PATH=$PYROOT/lib/python3.7/site-packages/ccdc/_lib
+#  Note:  similar vscode defintions are in ./.env
 #
 #  Packages recommended for some of the extra ccdc examples but not required for this package.
-# pip install matplotlib jinja2 numpy biopython
+#     pip install matplotlib jinja2 numpy biopython
 #
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Using macos settings'
+     #  Note:  the vscode defintions in ./.env
+     export CSDHOME=/Applications/CCDC/CSD_2021
+     PYROOT=$PYENV_ROOT/versions/3.7.9
+     export CSD_PYTHON_ROOT_PATH=$PYROOT
+     #
+     # For Linux
+     export LD_LIBRARY_PATH=$PYROOT/lib:$PYROOT/lib/python3.7/site-packages/ccdc/_lib:$LD_LIBRARY_PATH
+     # For Macos
+     export DYLD_FRAMEWORK_PATH=$PYROOT/lib/python3.7/site-packages/ccdc/_lib
+     export DYLD_LIBRARY_PATH=$PYROOT/lib/python3.7/site-packages/ccdc/_lib
+     #
+     ;;
+
+   Linux)
+     echo 'Using Linux settings'
+     export CSDHOME=/ssd/reference/cambridge-2021/CSD_2021
+     export PYROOT=$PYENV_ROOT/versions/3.7.9
+     export CSD_PYTHON_ROOT_PATH=$PYROOT
+     #
+     # For Linux
+     export LD_LIBRARY_PATH=$PYROOT/lib:$PYROOT/lib/python3.7/site-packages/ccdc/_lib:$LD_LIBRARY_PATH
+     # For Macos
+     export DYLD_FRAMEWORK_PATH=$PYROOT/lib/python3.7/site-packages/ccdc/_lib
+     export DYLD_LIBRARY_PATH=$PYROOT/lib/python3.7/site-packages/ccdc/_lib
+     #
+     ;;
+
+   # Add here more strings to compare
+   # See correspondence table at the bottom of this answer
+
+   *)
+     echo 'Unsupported OS - no settings'
+     ;;
+esac
